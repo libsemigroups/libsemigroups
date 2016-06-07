1d107f13b324d8aac8b3747951ca43ec06f459eb
James Mitchell <jdm3@st-andrews.ac.uk>
2016-06-07 10:29:06 +0100

    Add default value to RecVec

--- recvec.h
+++ recvec.h
@@ -27,11 +27,12 @@ class RecVec {
          * constructor
         ***********************************************************************/
 
-        explicit RecVec (size_t nr_cols, size_t nr_rows = 0)
+        explicit RecVec (size_t nr_cols, size_t nr_rows = 0, T default_val = 0)
           : _vec            (),
             _nr_used_cols   (nr_cols),
             _nr_unused_cols (0),
-            _nr_rows        (0)
+            _nr_rows        (0),
+            _default_val    (default_val)
         {
           this->add_rows(nr_rows);
         }
@@ -44,7 +45,8 @@ class RecVec {
           : _vec            (copy._vec),
             _nr_used_cols   (copy._nr_used_cols),
             _nr_unused_cols (copy._nr_unused_cols),
-            _nr_rows        (copy._nr_rows)
+            _nr_rows        (copy._nr_rows),
+            _default_val    (copy._default_val)
         {
           //std::cout << "RecVec: copy constructor!\n";
         }
@@ -57,7 +59,8 @@ class RecVec {
           : _vec            (copy->_vec),
             _nr_used_cols   (copy->_nr_used_cols),
             _nr_unused_cols (copy->_nr_unused_cols),
-            _nr_rows        (copy->_nr_rows)
+            _nr_rows        (copy->_nr_rows),
+            _default_val    (copy->_default_val)
         {
           //std::cout << "RecVec: copy constructor!\n";
           //assert((_nr_used_cols + _nr_unused_cols) * _nr_rows == _vec.size());
@@ -71,7 +74,8 @@ class RecVec {
           : _vec            (),
             _nr_used_cols   (copy._nr_used_cols),
             _nr_unused_cols (copy._nr_unused_cols),
-            _nr_rows        (copy.nr_rows()) {
+            _nr_rows        (copy.nr_rows()),
+            _default_val    (copy._default_val) {
 
           if (nr_cols_to_add <= _nr_unused_cols) {
             _vec = copy._vec;
@@ -93,7 +97,7 @@ class RecVec {
               _vec.push_back(copy.get(i, j));
             }
             for (; j < new_nr_cols; j++) {
-              _vec.push_back(static_cast<T>(0));
+              _vec.push_back(_default_val);
             }
           }
         }
@@ -110,7 +114,8 @@ class RecVec {
 
         void inline add_rows (size_t nr = 1) {
           _nr_rows += nr;
-          _vec.resize(_vec.size() + (_nr_used_cols + _nr_unused_cols) * nr, static_cast<T>(0));
+          _vec.resize(_vec.size() + (_nr_used_cols + _nr_unused_cols) * nr,
+                      _default_val);
         }
 
         /***********************************************************************
@@ -128,7 +133,7 @@ class RecVec {
           size_t old_nr_cols = _nr_used_cols + _nr_unused_cols;
           size_t new_nr_cols = std::max(5 * old_nr_cols / 4 + 4, nr + old_nr_cols);
 
-          _vec.resize(new_nr_cols * _nr_rows, static_cast<T>(0));
+          _vec.resize(new_nr_cols * _nr_rows, _default_val);
 
           typename std::vector<T>::iterator old_it(_vec.begin() + (old_nr_cols * _nr_rows) - old_nr_cols);
           typename std::vector<T>::iterator new_it(_vec.begin() + (new_nr_cols * _nr_rows) - new_nr_cols);
@@ -163,16 +168,15 @@ class RecVec {
         }
 
         /***********************************************************************
-         * clear: set all values to static_cast<T>(0), set number of used cols
+         * clear: set all values to _default_val, set number of used cols
          * to 0, does not reset the number of rows!!!
         ***********************************************************************/
 
         void inline clear () {
           size_t nr_cols = _nr_used_cols + _nr_unused_cols;
-          T val = static_cast<T>(0);
           for (size_t i = 0; i < _nr_rows; i++) {
             for (size_t j = 0; j < _nr_used_cols; j++) {
-              _vec[i * nr_cols + j] = val;
+              _vec[i * nr_cols + j] = _default_val;
             }
           }
           _nr_used_cols = 0;
@@ -215,6 +219,7 @@ class RecVec {
         size_t         _nr_used_cols;
         size_t         _nr_unused_cols;
         size_t         _nr_rows;
+        T              _default_val;
 };
 
 #endif  // BASICS_H_
