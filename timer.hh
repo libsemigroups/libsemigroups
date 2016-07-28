31ae92caa0b26a5aeb6d3f2e5b79de6d7bebd857
James Mitchell <jdm3@st-andrews.ac.uk>
2016-07-28 16:47:22 +0100

    Update timer.hh

--- timer.hh
+++ timer.hh
@@ -1,85 +1,86 @@
-/*
- * Semigroups GAP package
- *
- * This file contains a simple timer class.
- *
- */
+/*******************************************************************************
 
-#ifndef SRC_TIMER_H_
-#define SRC_TIMER_H_
+ Copyright (C) 2016 James D. Mitchell
+
+ This work is licensed under a Creative Commons Attribution-ShareAlike 4.0
+ International License. See
+ http://creativecommons.org/licenses/by-sa/4.0/
+
+ For a discussion about this code and latest version:
+
+*******************************************************************************/
+
+#ifndef TIMER_H_
+#define TIMER_H_
 
 #include <assert.h>
 #include <iostream>
 #include <chrono>
 
-using namespace std;
-using namespace std::chrono;
-
 class Timer {
 
-  typedef duration<long long int, std::nano>  nano_t;
-  typedef steady_clock::time_point  time_point_t;
+  typedef std::chrono::duration<long long int, std::nano>  nano_t;
+  typedef std::chrono::steady_clock::time_point            time_point_t;
 
  public:
-
     Timer () : _start(), _running(false) {}
 
     void start () {
       if (!_running) {
         _running = true;
-        _start = steady_clock::now();
+        _start = std::chrono::steady_clock::now();
       } else {
         assert(false);
       }
     }
 
-    void stop (bool report = true) {
+    void stop () {
+      print();
+      _running = false;
+    }
+
+    void print () {
       if (_running) {
-        time_point_t end     = steady_clock::now();
-        nano_t       elapsed = duration_cast<nano_t>(end - _start);
-        _running = false;
-        if (report) {
-          std::cout << "elapsed time = ";
-          if (print_it<hours>(elapsed, "h ", 0)) {
-            print_it<minutes>(elapsed, "m", 0);
-            std::cout << std::endl;
-            return;
-          } else if (print_it<minutes>(elapsed, "m ", 0)) {
-            print_it<seconds>(elapsed, "s", 0);
-            std::cout << std::endl;
-            return;
-          /*} else if (print_it<seconds>(elapsed, "s ", 0)) {
-            print_it<milliseconds>(elapsed, "ms", 0);
-            std::cout << std::endl;
-            return;*/
-          } else if (print_it<milliseconds>(elapsed, "ms ", 9)) {
-            std::cout << std::endl;
-            return;
-          } else if (print_it<microseconds>(elapsed, "us ", 9)) {
-            std::cout << std::endl;
-            return;
-          } else if (print_it<nanoseconds>(elapsed, "ns ", 0)) {
-            std::cout << std::endl;
-            return;
-          }
+        time_point_t end     = std::chrono::steady_clock::now();
+        nano_t       elapsed = std::chrono::duration_cast<nano_t>(end - _start);
+        std::cout << "elapsed time = ";
+        if (print_it<std::chrono::hours>(elapsed, "h ", 0)) {
+          print_it<std::chrono::minutes>(elapsed, "m", 0);
+          std::cout << std::endl;
+          return;
+        } else if (print_it<std::chrono::minutes>(elapsed, "m ", 0)) {
+          print_it<std::chrono::seconds>(elapsed, "s", 0);
+          std::cout << std::endl;
+          return;
+        } else if (print_it<std::chrono::milliseconds>(elapsed, "ms ", 9)) {
+          std::cout << std::endl;
+          return;
+        } else if (print_it<std::chrono::microseconds>(elapsed, "us ", 9)) {
+          std::cout << std::endl;
+          return;
+        } else if (print_it<std::chrono::nanoseconds>(elapsed, "ns ", 0)) {
+          std::cout << std::endl;
+          return;
         }
+      } else {
+        assert(false);
       }
     }
 
  private:
-   steady_clock::time_point _start;
-   bool                     _running;
+    std::chrono::steady_clock::time_point _start;
+    bool                                  _running;
 
-   template<typename T>
-     bool print_it (nano_t& elapsed, std::string str, size_t threshold) {
-       T x = duration_cast<T>(elapsed);
-       if (x > T(threshold)) {
-         std::cout << x.count() << str;
-         elapsed -= x;
-         return true;
-       }
-       return false;
-     }
+    template<typename T>
+      bool print_it (nano_t& elapsed, std::string str, size_t threshold) {
+        T x = std::chrono::duration_cast<T>(elapsed);
+        if (x > T(threshold)) {
+          std::cout << x.count() << str;
+          elapsed -= x;
+          return true;
+        }
+        return false;
+      }
 };
 
-#endif //SRC_TIMER_H_
+#endif // TIMER_H_
