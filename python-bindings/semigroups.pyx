d89202964354bdccef679b95139fc46e403a6a58
Nicolas M. Thiéry <nthiery@users.sf.net>
2017-01-17 00:28:55 +0100

    Preliminary PySemigroup wrapper class

--- python-bindings/semigroups.pyx
+++ python-bindings/semigroups.pyx
@@ -13,9 +13,35 @@ cdef extern from "semigroups.h" namespace "semigroupsplusplus":
         int size()
 
 def f():
-    cdef vector[Element *] gens;
+    cdef vector[Element *] gens
     gens.push_back(new Transformation[uint16_t]([0, 1, 0]))
     gens.push_back(new Transformation[uint16_t]([0, 1, 2]))
 
     S = new Semigroup(gens);
     return S.size()
+
+cdef class PySemigroup:
+    cdef Semigroup* c_semigroup      # hold a pointer to the C++ instance which we're wrapping
+    def __cinit__(self, generators):
+        # Type checking on the generators to decide on whether to build Transformation's, PartialTransformation, ...
+        # Add some plugins so that Sage can configure other converters
+        cdef vector[Element *] gens
+        for g in generators:
+            gens.push_back(new Transformation[uint16_t](g))
+        self.c_semigroup = new Semigroup(gens)
+
+    def __dealloc__(self):
+        del self.c_semigroup
+
+    def size(self):
+        """
+        Return the size of this semigroup
+
+        EXAMPLES::
+
+            sage: from semigroups import PySemigroup
+            sage: S = PySemigroup([[1,1,4,5,4,5],[2,3,2,3,5,5]])
+            sage: S.size()
+            5
+        """
+        return self.c_semigroup.size()
