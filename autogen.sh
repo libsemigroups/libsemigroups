e71ba754416bf5609db489d4824ebe49f58d47cc
James Mitchell <jdm3@st-and.ac.uk>
2017-01-16 16:50:52 +0000

    Temp: trying to get this to work

---
+++ autogen.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -ex
+#
+# Semigroups package for GAP
+#
+# This file is part of the build system of a GAP kernel extension.
+# Requires GNU autoconf, GNU automake and GNU libtool.
+#
+autoreconf -vif `dirname "$0"`
