c115bf128d7378fe791146933f4c71f1bef6e846
Nicolas M. Thiéry <nthiery@users.sf.net>
2017-01-17 01:39:47 +0100

    Draft of support of various types of elements through plugable converters

--- python-bindings/semigroups.pyx
+++ python-bindings/semigroups.pyx
@@ -8,6 +8,7 @@ cdef extern from "semigroups.h" namespace "semigroupsplusplus":
         pass
     cdef cppclass Transformation[T](Element):
         Transformation(vector[T]) except +
+        vector[T] _vector
     cdef cppclass Semigroup:
         Semigroup(vector[Element*]) except +
         int size()
@@ -20,14 +21,44 @@ def f():
     S = new Semigroup(gens);
     return S.size()
 
+cdef class Converter:
+    def is_a(self, t):
+        pass
+
+    cdef Element* convert_from(self, t):
+        pass
+
+    cdef convert_to(self, Element* t):
+        pass
+
+cdef class ListConverter(Converter):
+    def is_a(self, t):
+        return isinstance(t, (list, tuple))
+
+    cdef Element* convert_from(self, t):
+        return new Transformation[uint16_t](t)
+
+    cdef convert_to(self, Element * element):
+        cdef Transformation[uint16_t]* t = <Transformation[uint16_t]*> element
+        return [] # list((t[0]._vector)[0]) # TODO: Fixed that
+
+converters = [ListConverter()]
+
 cdef class PySemigroup:
-    cdef Semigroup* c_semigroup      # hold a pointer to the C++ instance which we're wrapping
+    cdef Semigroup* c_semigroup      # holds a pointer to the C++ instance which we're wrapping
+    cdef Converter converter         # object that contains pointers to conversion functions
     def __cinit__(self, generators):
         # Type checking on the generators to decide on whether to build Transformation's, PartialTransformation, ...
         # Add some plugins so that Sage can configure other converters
         cdef vector[Element *] gens
+        cdef converter
+        for conv in converters:
+            converter = <Converter>(conv)
+            if all(converter.is_a(g) for g in generators):
+                self.converter=converter
+                break
         for g in generators:
-            gens.push_back(new Transformation[uint16_t](g))
+            gens.push_back(self.converter.convert_from(g))
         self.c_semigroup = new Semigroup(gens)
 
     def __dealloc__(self):
