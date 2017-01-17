4d7341df10f245a501ef7ce4860899dd8c64b553
Nicolas M. Thiéry <nthiery@users.sf.net>
2017-01-17 16:15:04 +0100

    Changed Semigroup to take a list of Element as input.
    Bye bye the converter idea; it wasn't a good one anyway.

--- python-bindings/semigroups.pyx
+++ python-bindings/semigroups.pyx
@@ -4,29 +4,6 @@ from libc.stdint cimport uint16_t
 from libcpp.vector cimport vector
 cimport semigroups_cpp as cpp
 
-cdef class Converter:
-    def is_a(self, t):
-        pass
-
-    cdef cpp.Element* convert_from(self, t):
-        pass
-
-    cdef convert_to(self, cpp.Element* t):
-        pass
-
-cdef class ListConverter(Converter):
-    def is_a(self, t):
-        return isinstance(t, (list, tuple))
-
-    cdef cpp.Element* convert_from(self, t):
-        return new cpp.Transformation[uint16_t](t)
-
-    cdef convert_to(self, cpp.Element * element):
-        cdef cpp.Transformation[uint16_t]* t = <cpp.Transformation[uint16_t]*> element
-        return [] # list((t[0]._vector)[0]) # TODO: Fixed that
-
-converters = [ListConverter()]
-
 cdef class Element:
     cdef cpp.Element* cpp_element
 
@@ -67,19 +44,10 @@ cdef class Transformation(Element):
 
 cdef class Semigroup:
     cdef cpp.Semigroup* cpp_semigroup      # holds a pointer to the C++ instance which we're wrapping
-    cdef Converter converter         # object that contains pointers to conversion functions
     def __cinit__(self, generators):
-        # Type checking on the generators to decide on whether to build Transformation's, PartialTransformation, ...
-        # Add some plugins so that Sage can configure other converters
         cdef vector[cpp.Element *] gens
-        cdef converter
-        for conv in converters:
-            converter = <Converter>(conv)
-            if all(converter.is_a(g) for g in generators):
-                self.converter=converter
-                break
         for g in generators:
-            gens.push_back(self.converter.convert_from(g))
+            gens.push_back((<Element>g).cpp_element)
         self.cpp_semigroup = new cpp.Semigroup(gens)
 
     def __dealloc__(self):
@@ -91,9 +59,9 @@ cdef class Semigroup:
 
         EXAMPLES::
 
-            sage: from semigroups import Semigroup
-            sage: S = Semigroup([[1,1,4,5,4,5],[2,3,2,3,5,5]])
-            sage: S.size()
+            >>> from semigroups import Semigroup, Transformation
+            >>> S = Semigroup([Transformation([1,1,4,5,4,5]),Transformation([2,3,2,3,5,5])])
+            >>> S.size()
             5
         """
         return self.cpp_semigroup.size()
