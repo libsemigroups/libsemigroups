40e1b6bfd013493f77be6612c50b1376a4a5b14b
Alex Levine <aal20@st-andrews.ac.uk>
2017-03-29 20:03:24 +0100

    Add test_mul function to PartialPerm unit tests

--- python-bindings/tests/PartialPerm.test.py
+++ python-bindings/tests/PartialPerm.test.py
@@ -34,5 +34,14 @@ class TestPartialPerm(unittest.TestCase):
             PartialPerm([1,2],[0,1,2],3)
 
 
+    def test_mul(self):
+        assert PartialPerm([0, 1], [0, 1], 2)*PartialPerm([0, 1], [0, 1], 2)==PartialPerm([0, 1], [0, 1], 2)
+        assert PartialPerm([1, 2, 4, 6, 7, 23], [0, 5, 2, 4, 6, 7], 26)*PartialPerm([2, 4, 3, 5, 0, 19], [7, 8, 2, 3, 23, 0], 26)==PartialPerm([1, 2, 4, 6], [23, 3, 7, 8], 26)
+        assert PartialPerm([0, 3, 7, 2], [5, 7, 1, 3], 8)*PartialPerm([4, 7, 3, 6], [5, 0, 3, 2], 8)==PartialPerm([2, 3], [3, 0], 8)
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
