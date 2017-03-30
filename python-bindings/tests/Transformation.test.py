15c6ef2b1eb7e3a8db04e2f338d32399ddde034c
Alex Levine <aal20@st-andrews.ac.uk>
2017-03-30 21:48:25 +0100

    Add TestTransformation class to Transformation unittest with test_init function

--- python-bindings/tests/Transformation.test.py
+++ python-bindings/tests/Transformation.test.py
@@ -11,7 +11,11 @@ from semigroups import Transformation
 import semigroups
 
 
-
+class TestTransformation(unittest.TestCase):
+    def test_init(self):
+        Transformation([0, 1, 2, 3])
+        Transformation([1,1,3,2,4,3])
+        Transformation([9,3,1,2,0,8,1,2,0,5])
 
 
 
