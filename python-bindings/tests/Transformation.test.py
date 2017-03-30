28c61151d011e1da9fb56b15cb84477a0daec350
Alex Levine <aal20@st-andrews.ac.uk>
2017-03-30 21:47:32 +0100

    Add unit test file for Transformation class

---
+++ python-bindings/tests/Transformation.test.py
@@ -0,0 +1,23 @@
+import unittest
+import sys
+import os
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+del path
+
+from semigroups import Transformation
+import semigroups
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
