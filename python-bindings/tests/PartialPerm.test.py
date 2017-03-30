d6dca14780f3ed5fd3a9f42633d3a8122ba4ae62
Alex Levine <aal20@st-andrews.ac.uk>
2017-03-30 21:43:23 +0100

    Replace assertEquals with assertEqual

--- python-bindings/tests/PartialPerm.test.py
+++ python-bindings/tests/PartialPerm.test.py
@@ -37,9 +37,9 @@ class TestPartialPerm(unittest.TestCase):
 
 
     def test_mul(self):
-        self.assertEquals( PartialPerm([0, 1], [0, 1], 2)*PartialPerm([0, 1], [0, 1], 2), PartialPerm([0, 1], [0, 1], 2))
-        self.assertEquals( PartialPerm([1, 2, 4, 6, 7, 23], [0, 5, 2, 4, 6, 7], 26)*PartialPerm([2, 4, 3, 5, 0, 19], [7, 8, 2, 3, 23, 0], 26), PartialPerm([1, 2, 4, 6], [23, 3, 7, 8], 26))
-        self.assertEquals( PartialPerm([0, 3, 7, 2], [5, 7, 1, 3], 8)*PartialPerm([4, 7, 3, 6], [5, 0, 3, 2], 8), PartialPerm([2, 3], [3, 0], 8))
+        self.assertEqual( PartialPerm([0, 1], [0, 1], 2)*PartialPerm([0, 1], [0, 1], 2), PartialPerm([0, 1], [0, 1], 2))
+        self.assertEqual( PartialPerm([1, 2, 4, 6, 7, 23], [0, 5, 2, 4, 6, 7], 26)*PartialPerm([2, 4, 3, 5, 0, 19], [7, 8, 2, 3, 23, 0], 26), PartialPerm([1, 2, 4, 6], [23, 3, 7, 8], 26))
+        self.assertEqual( PartialPerm([0, 3, 7, 2], [5, 7, 1, 3], 8)*PartialPerm([4, 7, 3, 6], [5, 0, 3, 2], 8), PartialPerm([2, 3], [3, 0], 8))
 
         with self.assertRaises(AssertionError):
             PartialPerm([1,2],[0,1],3)*PartialPerm([1,2],[0,1],4)
