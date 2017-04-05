9efcd8236d117a9d263ee308cd00f64c34356d6e
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-05 16:34:50 +0100

    Add Bipartition test class with basic test_init function

---
+++ python-bindings/tests/Bipartiton.test.py
@@ -0,0 +1,26 @@
+import unittest
+import sys
+import os
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+del path
+
+from semigroups import Bipartition
+
+
+class TestPartialPerm(unittest.TestCase):
+    def test_init(self):
+        Bipartition([-1,-2],[2,-3],[1,3])
+        Bipartition([-7,-6,-5,-4],[3,2,1],[-3,-2,-1,4,5,6,7])
+        Bipartition([1,-1,2,-2])
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
