48e48005318f4a7b70ff2a7dd48be0174c24f6df
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-09 16:48:50 +0100

    Add test_mul function to Transformation unit tests

--- python-bindings/tests/Transformation.test.py
+++ python-bindings/tests/Transformation.test.py
@@ -7,7 +7,7 @@ if not path in sys.path:
     sys.path.insert(1, path)
 del path
 
-from semigroups import Transformation
+from semigroups import Transformation, PartialPerm
 import semigroups
 
 
@@ -25,10 +25,14 @@ class TestTransformation(unittest.TestCase):
         with self.assertRaises(TypeError):
             Transformation(26)
 
+    def test_mul(self):
+        self.assertEqual(Transformation([1, 3, 2, 1]) * Transformation([0, 3, 2, 2]), Transformation([3, 2, 2, 3]))
+        self.assertEqual(Transformation([2, 2, 2]) * Transformation([1, 0, 1]), Transformation([1, 1, 1]))
+        self.assertEqual(Transformation([0, 1, 2, 3, 4, 5]) * Transformation([3, 2, 2, 3, 1, 4]), Transformation([3, 2, 2, 3, 1, 4]))
 
-
-
-
+        with self.assertRaises(AssertionError):
+            Transformation([0, 2, 1]) * PartialPerm([0, 1], [1, 2], 3)
+            Transformation([0, 2, 1]) * Transformation([1, 2, 3, 0])
 
 
 
