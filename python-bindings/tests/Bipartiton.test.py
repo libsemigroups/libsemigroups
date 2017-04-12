a11792e81dd7928bdbf030ca75bf4084eba254cf
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-12 14:26:44 +0100

    Format changes to Bipartition unittests code

--- python-bindings/tests/Bipartiton.test.py
+++ python-bindings/tests/Bipartiton.test.py
@@ -9,26 +9,20 @@ del path
 
 from semigroups import Bipartition
 
-
 class TestPartialPerm(unittest.TestCase):
     def test_init(self):
-        Bipartition([-1,-2],[2,-3],[1,3])
-        Bipartition([-7,-6,-5,-4],[3,2,1],[-3,-2,-1,4,5,6,7])
-        Bipartition([1,-1,2,-2])
+        Bipartition([-1, -2], [2, -3], [1, 3])
+        Bipartition([-7, -6, -5, -4], [3, 2, 1], [-3, -2, -1, 4, 5, 6, 7])
+        Bipartition([1, -1, 2, -2])
 
     def test_init_fail(self):
         with self.assertRaises(ValueError):
-            Bipartition([1,-1,2])
-            Bipartition([1,2,3],[-3,-2])
+            Bipartition([1, -1, 2])
+            Bipartition([1, 2, 3], [-3, -2])
 
         with self.assertRaises(TypeError):
-            Bipartition([1,2,3],(-1,-2,-3))
-            Bipartition(1,2,-1,-2)
-
-
-
-
-
+            Bipartition([1, 2, 3], (-1, -2, -3))
+            Bipartition(1, 2, -1, -2)
 
 if __name__ == '__main__':
     unittest.main()
