8f92a433e1c4c28e2b12823dcc2d312640cf2f8b
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-12 14:48:26 +0100

    Add test_pow function to Bipartition unittests

--- python-bindings/tests/Bipartiton.test.py
+++ python-bindings/tests/Bipartiton.test.py
@@ -37,5 +37,18 @@ class TestPartialPerm(unittest.TestCase):
         with self.assertRaises(ValueError):
             Bipartition([1, -1], [2, 3, -3], [-2]) * Bipartition([1, -1, 2, -2])
 
+    def test_pow(self):
+        self.assertEqual(Bipartition([-7, -6, -5, -4], [3, 2, 1], [-3, -2, -1, 4, 5, 6, 7]) ** 20, Bipartition([1, 2, 3], [4, 5, 6, 7], [-1, -2, -3], [-4, -5, -6, -7]))
+        self.assertEqual(Bipartition([1, -1, 2, -2]) ** 26, Bipartition([1, 2, -1, -2]))
+        self.assertEqual(Bipartition([-1, -2], [2, -3], [1, 3]) ** 3, Bipartition([1, 3], [2, -3], [-1, -2]))
+ 
+        with self.assertRaises(ValueError):
+            Bipartition([1, -1, 2, -2]) ** -26
+            Bipartition([1], [-1, 2, -2]) ** 0
+
+        with self.assertRaises(TypeError):
+            Bipartition([1, 2], [-1], [-2]) ** 3.1415926535897932384626433832795028841971693993751058209
+            Bipartition([1, 2], [-1], [-2]) ** 'c'
+
 if __name__ == '__main__':
     unittest.main()
