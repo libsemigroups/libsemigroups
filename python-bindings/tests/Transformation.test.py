0f3696fd180fad866bfb62e1def4f0e37767876f
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-12 12:59:23 +0100

    Add test_pow function to Transformation unittest

--- python-bindings/tests/Transformation.test.py
+++ python-bindings/tests/Transformation.test.py
@@ -30,11 +30,24 @@ class TestTransformation(unittest.TestCase):
         self.assertEqual(Transformation([2, 2, 2]) * Transformation([1, 0, 1]), Transformation([1, 1, 1]))
         self.assertEqual(Transformation([0, 1, 2, 3, 4, 5]) * Transformation([3, 2, 2, 3, 1, 4]), Transformation([3, 2, 2, 3, 1, 4]))
 
-        with self.assertRaises(AssertionError):
+        with self.assertRaises(TypeError):
             Transformation([0, 2, 1]) * PartialPerm([0, 1], [1, 2], 3)
+
+        with self.assertRaises(ValueError):
             Transformation([0, 2, 1]) * Transformation([1, 2, 3, 0])
 
+    def test_pow(self):
+        self.assertEqual(Transformation([9, 3, 1, 2, 0, 8, 1, 2, 0, 5]) ** 6, Transformation([5, 1, 2, 3, 9, 0, 2, 3, 9, 8]))
+        self.assertEqual(Transformation([2, 2, 2]) ** 30, Transformation([2, 2, 2]))
+        self.assertEqual(Transformation([1, 2, 3, 3]) ** 8, Transformation([3, 3, 3, 3]))
+ 
+        with self.assertRaises(ValueError):
+            Transformation([1, 2, 3, 0])**-1
+            Transformation([1, 1, 3, 2, 4, 3]) ** 0
 
+        with self.assertRaises(TypeError):
+            Transformation([1, 0, 1, 2]) ** 1.5
+            Transformation([3, 2, 0, 0]) ** 'l'
 
 if __name__ == '__main__':
     unittest.main()
