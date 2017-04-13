e7139314ed92268096670750fcc57eca82091a83
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-13 10:17:58 +0100

    Add test_richcmp function to Bipartition unittests

--- python-bindings/tests/Bipartiton.test.py
+++ python-bindings/tests/Bipartiton.test.py
@@ -24,6 +24,15 @@ class TestPartialPerm(unittest.TestCase):
             Bipartition([1, 2, 3], (-1, -2, -3))
             Bipartition(1, 2, -1, -2)
 
+    def test_richcmp(self):
+        assert Bipartition([3, -4, -1], [2, -3], [4, -2], [1]) == Bipartition([4, -2], [3, -4, -1], [1], [2, -3])
+        assert not Bipartition([3, -4, -1], [2, -3], [4, -2], [1]) != Bipartition([4, -2], [3, -4, -1], [1], [2, -3])
+        assert Bipartition([1, -1, 3], [-3, 2, -2]) <= Bipartition([1, -1], [2, 3, -2], [-3])
+        assert Bipartition([1, -1], [2, -2]) < Bipartition([1, -2], [2, -1])
+        assert Bipartition([1, -1], [2, -2]) <= Bipartition([1, -2], [2, -1])
+        assert Bipartition([1, -1, 3], [-3, 2, -2]) >= Bipartition([1, -2], [2, -1])
+        assert not Bipartition([1, -1, 3], [-3, 2, -2]) > Bipartition([1, -1], [2, 3, -2], [-3])
+
     def test_mul(self):
         self.assertEqual(Bipartition([1, -1, 2, -2]) * Bipartition([1, -1, 2, -2]), Bipartition([1, 2, -1, -2]))
         self.assertEqual(Bipartition([1, 2], [-1], [-2]) * Bipartition([1, -1], [2, -2]), Bipartition([1, 2], [-1], [-2]))
