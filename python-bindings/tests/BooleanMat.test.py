4ba343eeccf48d8d0662e25bf68828728e5adbd4
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-13 21:49:34 +0100

    Add test_richcmp function to BooleanMat unittests

--- python-bindings/tests/BooleanMat.test.py
+++ python-bindings/tests/BooleanMat.test.py
@@ -7,7 +7,7 @@ if not path in sys.path:
     sys.path.insert(1, path)
 del path
 
-from semigroups import BooleanMat
+from semigroups import BooleanMat, Bipartition, Transformation, PartialPerm
 
 class TestPartialPerm(unittest.TestCase):
     def test_init(self):
@@ -27,7 +27,23 @@ class TestPartialPerm(unittest.TestCase):
         with self.assertRaises(ValueError):
             BooleanMat([True], [False])
             BooleanMat([True, False], [False, False], [True, True])
-            BooleanMat([True, True, False], [False, False])        
+            BooleanMat([True, True, False], [False, False])
+
+    def test_richcmp(self):
+        assert BooleanMat([True, False], [False, True]) == BooleanMat([True, False], [False, True])
+        assert not BooleanMat([True, False], [False, True]) != BooleanMat([True, False], [False, True])
+        assert not BooleanMat([True, False], [False, True]) == BooleanMat([False, False], [False, True])
+        assert BooleanMat([False]) < BooleanMat([True])
+        assert not BooleanMat([False, True, True], [True, True, False], [False, False, False]) < BooleanMat([False, True, False], [True, False, False], [False, False, True])
+        assert BooleanMat([False]) <= BooleanMat([False])
+        assert BooleanMat([True, False], [False, True]) > BooleanMat([True, False], [False, False])
+        assert not BooleanMat([True, False], [False, True]) > BooleanMat([True, False], [False, True])
+        assert BooleanMat([False]) >= BooleanMat([False])
+
+        with self.assertRaises(TypeError):
+            Bipartition([1, -2], [-1, 2]) >= BooleanMat([False, False], [True, False])
+            BooleanMat([False, False], [True, False]) < Transformation([0, 1])
+            BooleanMat([True, False], [False, True]) == PartialPerm([0], [1], 2)
 
 if __name__ == '__main__':
     unittest.main()
