9fb082ba60666973f90ba691e5c8c04970597b88
James Mitchell <jdm3@st-and.ac.uk>
2017-04-18 18:38:23 -0400

    py: initial python-bindings

---
+++ python-bindings/README.md
@@ -0,0 +1,37 @@
+# Python bindings for libsemigroups
+
+## Basic instructions
+
+    git clone ...
+
+    cd libsemigroups
+    make
+    sudo make install
+    sudo ldconfig
+
+    cd python-bindings
+    pip3 install --user . --upgrade
+
+    python3
+
+    >>> from semigroups import Semigroup, Transformation
+    >>> S = Semigroup([Transformation([1,1,4,5,4,5]),Transformation([2,3,2,3,5,5])])
+    >>> S.size()
+    5
+
+## Trick to debug segmentation faults
+
+Install Sage
+
+Install gdb in Sage:
+
+    sage -i gdb
+
+Run sage as:
+
+    sage -gdb
+
+and type the commands that trigger the segfault. Then gdb will be
+fired automatically, allowing for analysing the stack trace.
+
+## [TODO list](TODO)
---
+++ python-bindings/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    version = "0.2.0",
+    name = "libsemigroups-python-bindings",
+    description='Python bindings for the libsemigroup mathematics library',
+    long_description=long_description,
+    url='https://github.com/james-d-mitchell/libsemigroups/tree/master/python-bindings',
+    author='James Mitchell and Nicolas M. Thiéry',
+    author_email='TODO',
+    license='GPLv2+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='Mathematics, semigroup theory',
+
+    ext_modules = cythonize([
+        Extension("semigroups",
+                      sources=["semigroups.pyx", "semigroups_cpp.cpp"],
+                      libraries=["semigroups"],
+                      language="c++",             # generate C++ code
+                      extra_compile_args=["-std=c++11"],
+                      )]),
+
+    setup_requires=['pytest-runner', 'pytest-cython'],
+    tests_require=['pytest'],
+)
