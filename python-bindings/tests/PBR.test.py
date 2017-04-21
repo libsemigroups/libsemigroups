4045521d28b905f9e31f99331f1fb700b02ba6a4
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-21 18:59:26 +0100

    Add unittests for PBR

--- python-bindings/tests/PBR.test.py
+++ python-bindings/tests/PBR.test.py
@@ -40,7 +40,8 @@ class TestPBR(unittest.TestCase):
         assert not PBR([[1, -1], [-2, -1, 2]], [[2], [-2]]) != \
                    PBR([[1, -1], [-2, -1, 2]], [[2], [-2]])
         assert PBR([[1, -1], [-2, -1, 2]], [[2], [-2]]) != \
-               PBR([[1, 2], [-2, 1, 2], [3, -3]], [[2, 1], [-2, 2, 1], [1, -1]])
+               PBR([[1, 2], [-2, 1, 2], [3, -3]], 
+                   [[2, 1], [-2, 2, 1], [1, -1]])
         assert PBR([[1], [1, 2, -1]], [[1], [2, -1, 1]]) < \
                PBR([[1], [2]], [[-1], [-2]])
         assert PBR([[1, -1, 3], [-2, -1, 2], [3, -2]], 
@@ -85,14 +86,74 @@ class TestPBR(unittest.TestCase):
             Transformation([0, 2, 1]) * \
             PBR([[1, -1, 3], [-2, -1, 2], [3, -2]], [[2], [-2], [1, -1, 2]])
         with self.assertRaises(TypeError):
-            PBR([[1, -1, 3], [-2, -1, 2], [3, -2]], [[2], [-2], [1, -1, 2]]) * \
+            PBR([[1, -1, 3], [-2, -1, 2], [3, -2]], 
+                [[2], [-2], [1, -1, 2]]) * \
             Bipartition([1, -1], [2, 3, -3], [-2])
         with self.assertRaises(TypeError):
             PBR([[1, -1]], [[1]]) * 0.142857
 
         with self.assertRaises(ValueError):
-            PBR([[1, -1, 3], [-2, -1, 2], [3, -2]], [[2], [-2], [1, -1, 2]]) * \
+            PBR([[1, -1, 3], [-2, -1, 2], [3, -2]], 
+                [[2], [-2], [1, -1, 2]]) * \
             PBR([[1, -1]], [[1]])
+
+    def test_pow(self):
+        self.assertEqual(PBR([[1, -1, 3], [-2, -1, 2], [3, -2]],
+                            [[2], [-2], [1, -1, 2]]) ** 5,
+                         PBR([[1, 2, 3, -1, -2], [2, -1, -2], [3, -2]],
+                             [[2], [-2], [1, 2, -2]]))
+        self.assertEqual(PBR([[1, -1]], [[1]]) ** 26, PBR([[1, -1]], [[1]]))
+        self.assertEqual(PBR([[-1, 1, 2], [2]], [[-1, 1], [-2, 2]]) ** 4,
+                         PBR([[1, 2, -1], [2]], [[1, 2, -1], [2, -2]]))
+ 
+        with self.assertRaises(ValueError):
+            PBR([[1, -1, 3], [-2, -1, 2], [3, -2]],
+                            [[2], [-2], [1, -1, 2]]) ** -26
+        with self.assertRaises(ValueError):
+            PBR([[1, 2, 3, -1, -2, -3], 
+                              [1, 2, 3, -1, -2, -3], [3, -2, -3]], 
+                              [[2], [3, -2, -3], [1, 2, 3, -2, -3]]) ** 0
+
+        with self.assertRaises(TypeError):
+            PBR([[1, -1]], [[1]]) ** 0.21813
+        with self.assertRaises(TypeError):
+            PBR([[1, -1]], [[1]]) ** 'a'
+
+    def test_dealloc(self):
+        A = PBR([[1, -1]], [[1]]), 
+        B = PBR([[1, -1, 3], [-2, -1, 2], [3, -2]], [[2], [-2], [1, -1, 2]])
+        del A, B
+        assert not 'A' in globals()
+        assert not 'B' in globals()
+
+    def test_degree(self):
+        self.assertEqual(PBR([[1, -1]], [[1]]).degree(), 1)
+        self.assertEqual(PBR([[1, 2, 3, -1, -2, -3], 
+                              [1, 2, 3, -1, -2, -3], [3, -2, -3]], 
+                              [[2], [3, -2, -3], [1, 2, 3, -2, -3]]).degree(), 3)
+        self.assertEqual(PBR([[1, -1], [-2, -1, 2]], [[2], [-2]]).degree(), 2)
+
+    def test_identity(self):
+        self.assertEqual(PBR([[-1, 1, 2], [2]], [[-1, 1], [-2, 2]]).identity(),
+                         PBR([[1], [2]], [[-1], [-2]]))
+
+        self.assertEqual(PBR([[1, 2, 3, -1, -2, -3], 
+                              [1, 2, 3, -1, -2, -3], [3, -2, -3]], 
+                              [[2], [3, -2, -3], [1, 2, 3, -2, -3]]).identity(), 
+                         PBR([[1], [2], [3]], [[-1], [-2], [-3]]))
+        self.assertEqual(PBR([[1, -1]], [[1]]).identity(), PBR([[1]], [[-1]]))
+
+    def test_repr(self):
+        self.assertEqual(eval(PBR([[-1, 1, 2], [2]], [[-1, 1], [-2, 2]]).__repr__()),
+                         PBR([[-1, 1, 2], [2]], [[-1, 1], [-2, 2]]))
+
+        self.assertEqual(eval(PBR([[1, 2, 3, -1, -2, -3], 
+                              [1, 2, 3, -1, -2, -3], [3, -2, -3]], 
+                              [[2], [3, -2, -3], [1, 2, 3, -2, -3]]).__repr__()), 
+                         PBR([[1, 2, 3, -1, -2, -3], 
+                              [1, 2, 3, -1, -2, -3], [3, -2, -3]], 
+                              [[2], [3, -2, -3], [1, 2, 3, -2, -3]]))
+        self.assertEqual(eval(PBR([[1, -1]], [[1]]).__repr__()), PBR([[1, -1]], [[1]]))
 
 if __name__ == '__main__':
     unittest.main()
