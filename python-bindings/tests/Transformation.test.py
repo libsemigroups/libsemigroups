3ee2bf539ff236b38874f4bf8fbf72e0be41ab17
Alex Levine <aal20@st-andrews.ac.uk>
2017-04-21 18:03:48 +0100

    Fix syntax Transformation unittests

--- python-bindings/tests/Transformation.test.py
+++ python-bindings/tests/Transformation.test.py
@@ -19,11 +19,14 @@ class TestTransformation(unittest.TestCase):
     def test_init_fail(self):
         with self.assertRaises(ValueError):
             Transformation([1, 5, 26])
+        with self.assertRaises(ValueError):
             Transformation([1])
 
         with self.assertRaises(TypeError):
             Transformation(26)
+        with self.assertRaises(TypeError):
             Transformation(['a', 'b'])
+        with self.assertRaises(TypeError):
             Transformation([0.1, 1.0])
 
     def test_richcmp(self):
@@ -39,7 +42,9 @@ class TestTransformation(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             Transformation([2, 2, 0]) == Bipartition([1, -1], [2, 3, -2], [-3])
+        with self.assertRaises(TypeError):
             Bipartition([1, -1], [2, -2]) < Transformation([0, 1])
+        with self.assertRaises(TypeError):
             Bipartition([1, -1], [2, -2]) != Transformation([0, 1])
 
     def test_mul(self):
@@ -54,7 +59,9 @@ class TestTransformation(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             Transformation([0, 2, 1]) * PartialPerm([0, 1], [1, 2], 3)
+        with self.assertRaises(TypeError):
             Transformation([0, 2, 1]) * Bipartition([1, -1], [2, 3, -3], [-2])
+        with self.assertRaises(TypeError):
             Transformation([0, 1, 2, 3, 4, 5]) * 8
 
         with self.assertRaises(ValueError):
@@ -70,17 +77,21 @@ class TestTransformation(unittest.TestCase):
  
         with self.assertRaises(ValueError):
             Transformation([1, 2, 3, 0]) ** -1
+        with self.assertRaises(ValueError):
             Transformation([1, 1, 3, 2, 4, 3]) ** 0
 
         with self.assertRaises(TypeError):
             Transformation([1, 0, 1, 2]) ** 1.5
+        with self.assertRaises(TypeError):
             Transformation([3, 2, 0, 0]) ** 'l'
 
     def test_dealloc(self):
         U, V = Transformation([1, 0, 1, 2]), Transformation([1, 1, 3, 2, 4, 3])
         del U, V
         with self.assertRaises(NameError):
-            U; V
+            V
+        with self.assertRaises(NameError):
+            U
 
     def test_identity(self):
         self.assertEqual(Transformation([9, 3, 1, 2, 0, 
