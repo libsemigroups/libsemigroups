467aa9f5283fa3403ecadb5f0652c50d74514c37
Michael Torpey <mct25@st-andrews.ac.uk>
2017-04-28 15:51:38 +0100

    Travis: add valgrind tests (failing)

---
+++ ci/travis-valgrind.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Install valgrind
+sudo apt-get install -y valgrind
+
+# Setup
+ci/travis-setup.sh
+
+# Print version
+echo "Valgrind version:"
+valgrind --version
+
+etc/debug.sh
+make lstest
+
+if [ ! -e log ]
+then
+    mkdir log
+fi
+touch log/valgrind-log.txt
+
+libtool --mode=execute valgrind --leak-check=full ./lstest -d yes [quick],[standard] 2>&1 | tee log/valgrind-log.txt
+
+echo
+( ! grep -i "Invalid" log/valgrind-log.txt )
+( ! grep -E "lost: [^0]" log/valgrind-log.txt )
