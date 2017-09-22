fbf7a277d4e1d63595851028f1ed351ef6c25a40
James Mitchell <jdm3@st-and.ac.uk>
2017-09-22 15:50:27 +0100

    travis: add configure with --enable-stats

--- ci/travis-standard.sh
+++ ci/travis-standard.sh
@@ -15,6 +15,11 @@ make clean
 ./configure --enable-debug
 make check
 
+# Check standard tests with extra code for statistics 
+make clean
+./configure --enable-stats
+make check
+
 # Check standard tests with assertions off
 make clean
 ./configure
