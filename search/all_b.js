235d610cc43d9b5aa4b7b0d1f1bde2cdbcd7213f
James Mitchell <jdm3@st-and.ac.uk>
2017-10-02 14:45:11 +0100

    Version 0.5.1

--- search/all_b.js
+++ search/all_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['libsemigroups_20_2d_20c_2b_2b_20library_20for_20semigroups_20and_20monoids_20_2d_20version_200_2e5_2e0',['libsemigroups - C++ library for semigroups and monoids - Version 0.5.0',['../index.html',1,'']]],
+  ['libsemigroups_20_2d_20c_2b_2b_20library_20for_20semigroups_20and_20monoids_20_2d_20version_200_2e5_2e1',['libsemigroups - C++ library for semigroups and monoids - Version 0.5.1',['../index.html',1,'']]],
   ['left',['left',['../classlibsemigroups_1_1_semigroup.html#ae20823e7ce21c92f412d22821e7a4c05',1,'libsemigroups::Semigroup']]],
   ['left_5fblocks',['left_blocks',['../classlibsemigroups_1_1_bipartition.html#a37a71b314e8f23f133c31833d243f73f',1,'libsemigroups::Bipartition']]],
   ['left_5fcayley_5fgraph_5fcopy',['left_cayley_graph_copy',['../classlibsemigroups_1_1_semigroup.html#a1fb14978c8c5a12ff021d4bc8724238d',1,'libsemigroups::Semigroup']]],
