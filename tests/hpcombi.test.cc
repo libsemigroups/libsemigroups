305a2d26943e5ff9fc01fadced4e028f26bab999
Florent Hivert <Florent.Hivert@lri.fr>
2018-01-10 12:52:45 +0100

    Renner(B, 5)

--- tests/hpcombi.test.cc
+++ tests/hpcombi.test.cc
@@ -23,6 +23,40 @@
 using namespace libsemigroups;
 using namespace HPCombi;
 
+struct Renner0Element : public PTransf16 {
+
+  using PTransf16::PTransf16;
+
+  Renner0Element operator *(Renner0Element y) {
+    Renner0Element minab, maxab, mask, b = permuted(y);
+    mask = _mm_cmplt_epi8(y, Perm16::one());
+    minab = _mm_min_epi8(v, b);
+    maxab = _mm_max_epi8(v, b);
+    return static_cast<epu8>(_mm_blendv_epi8(maxab, minab, mask)) |
+      (y.v == cst_epu8_0xFF);
+  }
+
+  size_t complexity() { return -1; }
+};
+
+namespace std {
+
+template <> struct hash<Renner0Element> {
+  inline size_t operator()(const Renner0Element &ar) const {
+    return hash<HPCombi::Vect16>()(ar);
+  }
+};
+
+}
+
+template <>
+size_t libsemigroups::Semigroup<
+  Renner0Element,
+  std::hash<Renner0Element>,
+  std::equal_to<Renner0Element>>::complexity(Renner0Element) const {
+  return -1;
+}
+
 TEST_CASE("HPCombi 01: Transf16", "[quick][hpcombi][finite][01]") {
   Semigroup<Transf16, std::hash<Transf16>, std::equal_to<Transf16>> S(
       {Transf16({1, 2, 0})});
@@ -44,6 +78,26 @@ TEST_CASE("HPCombi 02: Transf16", "[quick][hpcombi][finite][02]") {
   S.reserve(597369 * 2);
   S.set_report(false);
   REQUIRE(S.size() == 597369);
+}
+
+const uint8_t FF = 0xFF;
+
+TEST_CASE("HPCombi 03: Renner0", "[quick][hpcombi][finite][03]") {
+  Semigroup<Renner0Element, std::hash<Renner0Element>,
+            std::equal_to<Renner0Element>> S(
+      {
+        Renner0Element({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}),
+        Renner0Element({FF,FF,FF,FF,FF,FF,FF,FF, 8, 9,10,11,12,13,14,15}),
+          Renner0Element({ 0, 1, 2, 3, 4, 5, 6, 8, 7, 9,10,11,12,13,14,15}),
+          Renner0Element({0, 1, 2, 3, 4, 5, 7, 6, 9, 8,10,11,12,13,14,15}),
+          Renner0Element({0, 1, 2, 3, 4, 6, 5, 7, 8,10, 9,11,12,13,14,15}),
+          Renner0Element({0, 1, 2, 3, 5, 4, 6, 7, 8, 9,11,10,12,13,14,15}),
+          Renner0Element({0, 1, 2, 4, 3, 5, 6, 7, 8, 9,10,12,11,13,14,15}),
+          Renner0Element({0, 1, 3, 2, 4, 5, 6, 7, 8, 9,10,11,13,12,14,15})
+      });
+  S.set_report(true);
+  REQUIRE(S.size() == 8962225);
+  REQUIRE(S.nridempotents() == 128);
 }
 
 TEST_CASE("HPCombi 03: full transformation monoid 8",
