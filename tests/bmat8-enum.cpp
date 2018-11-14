79e1192a3925c66464b8bf3e8f23ff6e4e81b11e
Finlay Smith <fls3@st-andrews.ac.uk>
2018-11-14 14:26:56 +0000

    bmat8-enum: fixes

--- tests/bmat8-enum.cpp
+++ tests/bmat8-enum.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <unordered_set>  // for unordered_set
 #include <vector>         // for vector
 #include <x86intrin.h>
@@ -18,9 +19,21 @@ namespace bmat8_enum {
   static std::array<bool, 256>             row_seen;
   static std::vector<std::vector<uint8_t>> row_orb_by_row;
   static size_t                            first_row;
-  static int                               nr_ones;
+  static int                               min_ones;
 
-  BMat8 canonical_bmat8(BMat8 bm) {
+  BMat8 BMat8_from_rows(std::vector<uint8_t> const& rows) {
+    LIBSEMIGROUPS_ASSERT(rows.size() <= 8);
+    LIBSEMIGROUPS_ASSERT(0 < rows.size());
+    size_t out = 0;
+    size_t i   = 0;
+    for (uint8_t x : rows){
+      out |= (static_cast<size_t>(x) << 8 * (7 - i));
+      ++i;
+    }
+    return BMat8(out);
+  }
+
+  BMat8 canonical_bmat8(BMat8 bm, size_t n) {
     return bm;
   }
 
@@ -35,47 +48,12 @@ namespace bmat8_enum {
   int nr_ones(uint8_t x) {
     return __builtin_popcount(x);
   }
-
-  std::vector<BMat8>& bmat_enum(size_t dim) {
-    n        = dim;
-    max      = (1 << n) - 1;
-    rows     = std::vector<uint8_t>();
-    set      = std::unordered_set<BMat8>();
-    out      = std::vector<BMat8>();
-    row_seen = std::array<bool, 256>;
-    for (bool& x : row_seen) {
-      x = false;
-    }
-    row_seen[0]    = true;
-    row_orb_by_row = std::vector<std::vector<uint8_t>>();
-    dive(0);
-
-    for (size_t i = 0; i < n - 1; ++i) {
-      first_row = i;
-      for (size_t j = 1; j < n; ++j) {
-        uint8_t row = (1 << j) - 1;
-        rows[i]     = row;
-        nr_ones     = nr_ones(x);
-        row_orb_by_row[i].clear();
-        row_orb_by_row[i].push_back(0);
-        row_orb_by_row[i].push_back(row);
-        row_seen[row] = true;
-
-        dive(i + 1);
-
-        row_seen[row] = false;
-      }
-      rows[i] = 0;
-    }
-    out.push_back(2);
-    out.push_back(1);
-    return &out;
-  }
-
+  
   void dive(size_t k) {
-    if (0 <= k && k < n - 1) {
+    LIBSEMIGROUPS_ASSERT(k > 0);
+    if (k < n - 1) {
       for (size_t row = rows[k - 1]; row < max; ++row) {
-        if (!row_seen[row] && nr_ones(row) >= nr_ones) {
+        if (!row_seen[row] && nr_ones(row) >= min_ones) {
           rows[k] = row;
 
           // UPDATE ROW ORB
@@ -101,22 +79,60 @@ namespace bmat8_enum {
       }
     } else if (k == n - 1) {
       for (uint8_t row = rows[k - 1]; row <= max; ++row) {
-        if (!row_seen[row] && nr_ones(row) >= nr_ones) {
+        if (!row_seen[row] && nr_ones(row) >= min_ones) {
           rows[k] = row;
-          BMat8 bm(rows);
+          BMat8 bm = BMat8_from_rows(rows);
           // move the matrix to the right place
-          bm = BMat8(bm.to_int() << (9 * (8 - n)));
+          bm = BMat8(bm.to_int() << 4);
           if (is_col_reduced(bm)) {
+            std::cout << bm << std::endl;
             BMat8 canon = canonical_bmat8(bm, n);
-            if (set.find(canon) != set.end()) {
+            if (set.find(canon) == set.end()) {
               set.insert(canon);
               out.push_back(canon);
             }
+          } else {
           }
         }
       }
       rows[k] = 0;
     }
+  }
+
+  std::vector<BMat8>& bmat_enum(size_t dim) {
+    n        = dim;
+    max      = (1 << n) - 1;
+    rows     = std::vector<uint8_t>(n, 0);
+    set      = std::unordered_set<BMat8>();
+    out      = std::vector<BMat8>();
+    row_seen = std::array<bool, 256>();
+    for (bool& x : row_seen) {
+      x = false;
+    }
+    row_seen[0]    = true;
+    row_orb_by_row
+        = std::vector<std::vector<uint8_t>>(8, std::vector<uint8_t>());
+
+    for (size_t i = 0; i < n - 1; ++i) {
+      first_row = i;
+      for (size_t j = 1; j < n; ++j) {
+        uint8_t row = (1 << j) - 1;
+        rows[i]     = row;
+        min_ones     = nr_ones(row);
+        row_orb_by_row[i].clear();
+        row_orb_by_row[i].push_back(0);
+        row_orb_by_row[i].push_back(row);
+        row_seen[row] = true;
+
+        dive(i + 1);
+
+        row_seen[row] = false;
+      }
+      rows[i] = 0;
+    }
+    out.push_back(BMat8(1));
+    out.push_back(BMat8(0));
+    return out;
   }
 }  // namespace bmat8_enum
 
