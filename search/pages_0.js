f5907998781f34bc1ad0ae66e43e0d3945c16171
James D. Mitchell <jdm3@st-andrews.ac.uk>
2019-03-15 12:06:43 +0000

    Version 0.6.7

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['libsemigroups_20_2d_20c_2b_2b_20library_20for_20semigroups_20and_20monoids_20_2d_20version_200_2e6_2e6',['libsemigroups - C++ library for semigroups and monoids - Version 0.6.6',['../index.html',1,'']]]
+  ['libsemigroups_20_2d_20c_2b_2b_20library_20for_20semigroups_20and_20monoids_20_2d_20version_200_2e6_2e7',['libsemigroups - C++ library for semigroups and monoids - Version 0.6.7',['../index.html',1,'']]]
 ];
