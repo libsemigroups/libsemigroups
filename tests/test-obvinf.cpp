dd8e18df9345c08cdb12eabf1e9838bf3f8fccb0
Reinis Cirpons <rc234@st-andrews.ac.uk>
2020-04-16 00:32:11 +0100

    Add a matrix based condition to is_obviously_infinite, add tests and add them to the makefile.

---
+++ tests/test-obvinf.cpp
@@ -0,0 +1,67 @@
+// libsemigroups - C++ library for semigroups and monoids
+// Copyright (C) 2020 Reinis Cirpons 
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// TODO(now):
+// 1. add more tests 
+
+#include "libsemigroups/obvinf.hpp"
+
+#include "catch.hpp"  // for REQUIRE, REQUIRE_THROWS_AS, REQUI...
+#include "test-main.hpp"
+
+namespace libsemigroups {
+
+  LIBSEMIGROUPS_TEST_CASE("ObviouslyInfinite",
+                          "001",
+                          "description",
+                          "[quick]") {
+  detail::IsObviouslyInfinite<char, std::string> ioi(2);
+  std::vector<std::string> v = {"ab", "a", "aba", "ba"};
+  ioi.add_rules(v.cbegin(), v.cend());
+  REQUIRE(ioi.result());
+  }
+
+  LIBSEMIGROUPS_TEST_CASE("ObviouslyInfinite",
+                          "002",
+                          "description",
+                          "[quick]") {
+  detail::IsObviouslyInfinite<char, std::string> ioi(2);
+  std::vector<std::string> v = {"aba", "aa", "bb", "b", "abab", "abbba"};
+  ioi.add_rules(v.cbegin(), v.cend());
+  REQUIRE(ioi.result());
+  }
+
+  LIBSEMIGROUPS_TEST_CASE("ObviouslyInfinite",
+                          "003",
+                          "description",
+                          "[quick]") {
+  detail::IsObviouslyInfinite<char, std::string> ioi(3);
+  std::vector<std::string> v = {"aba", "bc", "ca", "b"};
+  ioi.add_rules(v.cbegin(), v.cend());
+  REQUIRE(ioi.result());
+  }
+
+  LIBSEMIGROUPS_TEST_CASE("ObviouslyInfinite",
+                          "004",
+                          "description",
+                          "[quick]") {
+  detail::IsObviouslyInfinite<char, std::string> ioi(2);
+  std::vector<std::string> v = {"aa", "bba", "bbaa", "bbbbbb"};
+  ioi.add_rules(v.cbegin(), v.cend());
+  REQUIRE(ioi.result());
+  }
+}  // namespace libsemigroups
