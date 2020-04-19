91e951c9e0239f76986ab593af994ba94bb4051d
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-04-19 19:26:13 +0100

    travis: use requirements.txt in doc

--- ci/travis-doc.sh
+++ ci/travis-doc.sh
@@ -7,15 +7,9 @@ ci/travis-setup-hpcombi.sh
 ./configure
 
 sudo -H pip3 install --upgrade pip
-
-sudo -H pip3 install sphinx 
-sudo -H pip3 install bs4 
+sudo -H pip3 install setuptools
+sudo -H pip3 install -r docs/requirements.txt
 sudo -H pip3 install --upgrade --ignore-installed pyyaml
-sudo -H pip3 install lxml 
-sudo -H pip3 install breathe 
-sudo -H pip3 install sphinx_rtd_theme 
-sudo -H pip3 install sphinx-copybutton 
-sudo -H pip3 install sphinxcontrib-bibtex
 
 echo "Doxygen version: "
 doxygen --version
