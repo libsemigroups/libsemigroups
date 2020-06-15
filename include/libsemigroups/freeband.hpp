7a2a4fc499b8ad5bb4467300686e02f1b8be5edd
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-06-15 18:29:36 +0100

    snap

--- include/libsemigroups/freeband.hpp
+++ include/libsemigroups/freeband.hpp
@@ -20,6 +20,12 @@
 #ifndef LIBSEMIGROUPS_FREEBAND_HPP_
 #define LIBSEMIGROUPS_FREEBAND_HPP_
 
+#include <algorithm>  // for max_element
+#include <iostream>   // for cout
+#include <vector>     // for vector
+
+#include "libsemigroups/constants.hpp"
+#include "libsemigroups/libsemigroups-debug.hpp"
 #include "types.hpp" // word_type
 
 namespace libsemigroups {
@@ -33,7 +39,52 @@ namespace libsemigroups {
     return freeband_equal_to(xx, yy);
   }
 
-  word_type right(word_type const& w, size_t const k);
+  void standardize(word_type& x);
+
+    template <typename T>
+    word_type right(T const first, T const last, size_t const k) {
+      // LIBSEMIGROUPS_ASSERT(is_standardized(first, last));
+      word_type out;
+      if (std::distance(first, last) == 0) {
+        return out;
+      }
+      T                   j            = first;
+      size_t              content_size = 0;
+      size_t const        N = *std::max_element(first, last) + 1;
+      std::vector<size_t> multiplicity(N + 1, 0);
+      for (auto i = first; i != last; ++i) {
+        if (i != first) {
+          LIBSEMIGROUPS_ASSERT(multiplicity[*(i- 1)] != 0);
+          --multiplicity[*(i - 1)];
+          if (multiplicity[*(i - 1)] == 0) {
+            --content_size;
+          }
+        }
+        while (j < last
+              && (multiplicity[*j] != 0 || content_size < k)) {
+          if (multiplicity[*j] == 0) {
+            ++content_size;
+          }
+          ++multiplicity[*j];
+          ++j;
+        }
+        out.push_back(content_size == k ? size_t(std::distance(first, j)) - 1
+                                        : UNDEFINED);
+      }
+      return out;
+    }
+
+    // Reverses and corrects output of "right" to "left"
+    word_type reverse(word_type&& out) {
+      std::reverse(out.begin(), out.end());
+      for (auto& x : out) {
+        if (x != UNDEFINED) {
+          x = out.size() - x - 1;
+        }
+      }
+      return std::move(out);
+    }
+
 
 }  // namespace libsemigroups
 
