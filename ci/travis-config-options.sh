66e51c338b07f5b42e0f07cd040540578b24260c
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-08-20 15:14:32 +0100

    travis: check verbose mode

--- ci/travis-config-options.sh
+++ ci/travis-config-options.sh
@@ -9,3 +9,6 @@ make test_all -j4 && ./test_all "[quick],[standard]"
 
 ./configure --disable-popcnt --disable-clzll 
 make test_bitset -j4 && ./test_bitset "[quick]"
+
+./configure --enable-verbose
+make test_all -j4 && ./test_all "[quick]"
