2581550114b119d82e49e0e2fe34e4f5bf7a8704
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-08-28 09:58:54 +0100

    travis: install valgrind with apt

--- ci/travis-valgrind.sh
+++ ci/travis-valgrind.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
-test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
-echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
-
-brew update
-brew install valgrind
-
 # Print version
 echo "Valgrind version:"
 valgrind --version
