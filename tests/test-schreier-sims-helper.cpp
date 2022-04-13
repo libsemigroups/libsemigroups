87c0118a58cd94be51dee8d21c9cb2af4ef5070a
reiniscirpons <rc234@st-andrews.ac.uk>
2022-04-13 16:17:05 +0100

    Add even more tests

--- tests/test-schreier-sims-helper.cpp
+++ tests/test-schreier-sims-helper.cpp
@@ -19,7 +19,6 @@
 
 #include <cstddef>  // for size_t
 #include <cstdint>  // for uint64_t
-#include <iostream>
 
 #include "catch.hpp"                 // for LIBSEMIGROUPS_TEST_CASE
 #include "libsemigroups/report.hpp"  // for ReportGuard
@@ -189,6 +188,43 @@ namespace libsemigroups {
     REQUIRE(U.contains(Perm({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 11, 12})));
     REQUIRE(U.contains(Perm({0, 4, 8, 1, 5, 9, 2, 6, 10, 3, 7, 11, 12})));
     REQUIRE(U.contains(Perm({11, 10, 5, 7, 8, 2, 9, 3, 4, 6, 1, 0, 12})));
+  }
+
+  LIBSEMIGROUPS_TEST_CASE("SchreierSimsHelper",
+                          "010",
+                          "S17 and A39 intersection",
+                          "[quick][schreier-sims]") {
+    auto             rg = ReportGuard(REPORT);
+    SchreierSims<50> S, T, U, UP;
+    using Perm   = SchreierSims<50>::element_type;
+    using One    = SchreierSims<50>::One;
+    using Action = SchreierSims<50>::Action;
+    S.add_generator(Perm({1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                          14, 15, 16, 0,  17, 18, 19, 20, 21, 22, 23, 24, 25,
+                          26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+                          39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}));
+    S.add_generator(Perm({1,  0,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+                          13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                          26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+                          39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}));
+    T.add_generator(Perm({1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                          14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                          27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 0,
+                          39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}));
+    T.add_generator(Perm({0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+                          13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                          26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 37, 38, 36,
+                          39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49}));
+    schreier_sims_helper::intersection(U, S, T);
+    REQUIRE(U.size() == 177843714048000);
+    REQUIRE(U.contains(Perm({1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                             14, 15, 16, 0,  17, 18, 19, 20, 21, 22, 23, 24, 25,
+                             26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+                             39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49})));
+    REQUIRE(U.contains(Perm({0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+                             13, 15, 16, 14, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                             26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+                             39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49})));
   }
 
 }  // namespace libsemigroups
