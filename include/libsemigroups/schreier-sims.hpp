0be3d3421e835a859ce8e9df46f68ffd4a947d95
Reinis Cirpons <43414125+reiniscirpons@users.noreply.github.com>
2022-06-09 16:19:31 +0300

    Update include/libsemigroups/schreier-sims.hpp
    Co-authored-by: James Mitchell <james-d-mitchell@users.noreply.github.com>

--- include/libsemigroups/schreier-sims.hpp
+++ include/libsemigroups/schreier-sims.hpp
@@ -333,8 +333,9 @@ namespace libsemigroups {
     //! \parameters
     //! (None)
     size_t number_of_strong_generators(index_type depth) const {
-      if (_base_size == 0)
+      if (_base_size == 0) {
         return 0;
+      }
       if (depth >= _base_size) {
         LIBSEMIGROUPS_EXCEPTION(
             "depth out of bounds, expected value in range [0, %d), got %d",
