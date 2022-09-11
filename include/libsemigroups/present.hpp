4c6288bd7eaab2d1c5508fe8b276eda8fa63e6b3
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-09-11 14:21:12 +0100

    present: remove auto return type

--- include/libsemigroups/present.hpp
+++ include/libsemigroups/present.hpp
@@ -698,7 +698,7 @@ namespace libsemigroups {
     //! \no_libsemigroups_except
     // TODO(later) complexity
     template <typename W>
-    auto longest_common_subword(Presentation<W>& p);
+    W longest_common_subword(Presentation<W>& p);
 
     //! Replace non-overlapping instances of a subword via iterators.
     //!
