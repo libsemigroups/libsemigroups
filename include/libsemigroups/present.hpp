5e3490b83d1bc76f34732fcc8cb248be15bbd061
Murray Whyte <mw231@st-andrews.ac.uk>
2022-10-10 16:09:32 +0100

    present doc: Fix inaccurate exception information for replace_subword

--- include/libsemigroups/present.hpp
+++ include/libsemigroups/present.hpp
@@ -761,8 +761,7 @@ namespace libsemigroups {
     //!
     //! \returns (None)
     //!
-    //! \exceptions
-    //! \no_libsemigroups_except
+    //! \throws LibsemigroupsException if `existing` is the empty word.
     // TODO(later) complexity
     template <typename W>
     void replace_subword(Presentation<W>& p,
