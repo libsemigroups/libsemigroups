099b3a7a5e7d01b623460c231430081547688f86
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-12-13 12:09:31 +0000

    etc: add tpp files to lint make target

--- etc/make-lint.sh
+++ etc/make-lint.sh
@@ -7,8 +7,9 @@ if [[ $# -ne 0 ]]; then
 fi
 
 find include/libsemigroups/*.hpp -type f -exec cpplint --repository='include' {} \; | tee lint.log
+find include/libsemigroups/*.tpp -type f -exec cpplint --repository='include' --extensions='tpp' {} \; | tee -a lint.log
 find src/*.hpp -type f -exec cpplint --repository='..' {} \; | tee -a lint.log
-find src/*.cpp -type f -exec cpplint --repository='include' {} \; | tee -a lint.log 
+find src/*.cpp -type f -exec cpplint --repository='include' {} \; | tee -a lint.log
 find tests/*.hpp -type f ! -iname "catch.hpp" -exec cpplint --repository='..' {} \; | tee -a lint.log
 find tests/*.cpp -type f -exec cpplint --repository='include' {} \; | tee -a lint.log
 find benchmarks/*.hpp -type f ! -iname "catch.hpp" -exec cpplint --repository='..' {} \; | tee -a lint.log
