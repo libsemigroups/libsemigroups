728ea0a7d30753fb9cd287b0f4add9714cca2321
James D. Mitchell <jdm3@st-andrews.ac.uk>
2023-02-02 16:34:41 +0000

    ci: update to use c++17 branch of Semigroups GAP

--- ci/run-gap-tests-in-docker-container.sh
+++ ci/run-gap-tests-in-docker-container.sh
@@ -7,8 +7,8 @@ sudo apt-get install git --yes
 sudo apt-get install libtool-bin --yes 
 
 # Next commands executed in the container...
-GAP_SEMIGROUPS_BRANCH=main
-GAP_SEMIGROUPS_REPO=semigroups
+GAP_SEMIGROUPS_BRANCH="c++17"
+GAP_SEMIGROUPS_REPO="james-d-mitchell"
 GAP_VERSION=$(ls inst)
 GAP_SH="$HOME/inst/$GAP_VERSION/bin/gap.sh"
 
