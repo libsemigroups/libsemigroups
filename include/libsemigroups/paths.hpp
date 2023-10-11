ab24acccde60daaaf981101c2afb587a35022a4e
Murray Whyte <mw231@st-andrews.ac.uk>
2023-10-11 16:34:04 +0100

    Add checks word graph is not null ptr

--- include/libsemigroups/paths.hpp
+++ include/libsemigroups/paths.hpp
@@ -915,12 +915,14 @@ namespace libsemigroups {
     mutable const_iterator      _end;
     mutable bool                _current_valid;
 
+    bool set_iterator_no_checks() const;
     bool set_iterator() const;
 
    public:
     output_type get() const {
       set_iterator();
-      return std::visit([](auto& it) -> auto const& { return *it; }, _current);
+      return std::visit(
+          [](auto& it) -> auto const& { return *it; }, _current);
     }
 
     void next() {
@@ -935,6 +937,7 @@ namespace libsemigroups {
     }
 
     uint64_t size_hint() const;
+    uint64_t size_hint_no_checks() const;
 
     uint64_t count() const {
       return size_hint();
@@ -1033,6 +1036,13 @@ namespace libsemigroups {
 
     template <typename Subclass>
     Subclass& order(Subclass* obj, Order val);
+
+   private:
+    void throw_if_word_graph_nullptr() const {
+      if (_digraph == nullptr) {
+        LIBSEMIGROUPS_EXCEPTION("No word graph defined");
+      }
+    }
   };
 
   template <typename Node>
