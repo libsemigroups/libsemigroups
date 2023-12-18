a8751e6a4f0df4cf2aa91b1668fc407653f36de4
James D. Mitchell <jdm3@st-andrews.ac.uk>
2023-12-18 17:04:15 +0000

    Compiles x 3

---
+++ include/libsemigroups/sims2.hpp
@@ -0,0 +1,157 @@
+//
+// libsemigroups - C++ library for semigroups and monoids
+// Copyright (C) 2023 James D. Mitchell
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// This file contains a declaration of a class for performing the "low-index
+// congruence" algorithm for 2-sided congruences of semigroups and monoids.
+
+// This file and the class Sims2 exist because iterating through 2-sided
+// congruences is fundamentally different that iterating through 1-sided
+// congruences. In more words, iterating through 2-sided congruences requires
+// some extra steps than iterating through 1-sided congruences. It might have
+// been more pleasing to allow Sims1 objects (maybe appropriately renamed) to
+// accept congruence_kind::twosided as their "kind". However, this would either
+// have required:
+// 1. run time checks in the iterator_base::try_define function (and probably
+//    elsewhere too) if we were enumerating 1-sided or 2-sided congruences.
+// 2. making iterator_base_1_sided and iterator_base_2_sided, and then
+//    templating iterator and thread_iterator to use the appropriate type
+//    depending on the value of "kind()".
+//
+// The disadvantage of 1 is that it would likely be slower, since try_define is
+// the critical function for Sims1. It also makes the code more complicated,
+// and breaks backwards compatibility. The disadvantage of 2 is that the return
+// type of "cbegin" and "cend" depends on whether we are iterating through 1- or
+// 2-sided congruences. In other words, number 2 above doesn't actually work.
+// Hence we've opted for just having a separate class for low-index 2-sided
+// congruences. This is also cleaner since we don't have to touch the impl of
+// Sims1 (much) and is more backwards compatible.
+
+#ifndef LIBSEMIGROUPS_SIMS2_HPP_
+#define LIBSEMIGROUPS_SIMS2_HPP_
+
+#include "sims1.hpp"
+
+namespace libsemigroups {
+
+  class Sims2 : public SimsBase<Sims2> {
+   public:
+    using node_type       = SimsBase::node_type;
+    using label_type      = SimsBase::label_type;
+    using letter_type     = SimsBase::letter_type;
+    using size_type       = SimsBase::size_type;
+    using word_graph_type = SimsBase::word_graph_type;
+
+    Sims2()                        = default;
+    Sims2(Sims2 const& other)      = default;
+    Sims2(Sims2&&)                 = default;
+    Sims2& operator=(Sims2 const&) = default;
+    Sims2& operator=(Sims2&&)      = default;
+    ~Sims2()                       = default;
+
+    Sims2& init() {
+      Sims1Settings<Sims2>::init();
+      return *this;
+    }
+
+    template <typename Word>
+    Sims2(Presentation<Word> p) : Sims2() {
+      presentation(p);
+    }
+
+   private:
+    using PendingDef = Sims1::PendingDef;
+
+    // This class collects some common aspects of the iterator and
+    // thread_iterator nested classes. The mutex does nothing for <iterator>
+    // and is an actual std::mutex for <thread_iterator>.
+    class iterator_base : public Sims1::iterator_base<Sims2> {
+     public:
+      using const_reference = Sims1::iterator_base<Sims2>::const_reference;
+      using const_pointer   = Sims1::iterator_base<Sims2>::const_pointer;
+
+     protected:
+      std::vector<word_type> _2_sided_include;
+      std::vector<word_type> _2_sided_words;
+
+      using Sims1::iterator_base<Sims2>::init;
+      using Sims1::iterator_base<Sims2>::try_pop;
+
+      // We could use the copy constructor, but there's no point in copying
+      // anything except the FelschGraph and so we only copy that.
+      // TODO rename
+      void copy_felsch_graph(iterator_base const& that) {
+        Sims1::iterator_base<Sims2>::copy_felsch_graph(that);
+        _2_sided_include = that._2_sided_include;
+        _2_sided_words   = that._2_sided_words;
+      }
+
+      // Try to make the definition represented by PendingDef, returns false if
+      // it wasn't possible, and true if it was.
+      //! No doc
+      [[nodiscard]] bool try_define(PendingDef const&);
+
+     public:
+      iterator_base(Sims2 const* s, size_type n);
+
+      // None of the constructors are noexcept because the corresponding
+      // constructors for Presentation aren't currently
+
+      iterator_base() = default;
+
+      iterator_base(iterator_base const& that);
+      iterator_base(iterator_base&& that);
+      iterator_base& operator=(iterator_base const& that);
+      iterator_base& operator=(iterator_base&& that);
+      ~iterator_base();
+
+      using Sims1::iterator_base<Sims2>::operator==;
+      using Sims1::iterator_base<Sims2>::operator!=;
+      using Sims1::iterator_base<Sims2>::operator*;
+      using Sims1::iterator_base<Sims2>::operator->;
+
+      //! No doc
+      void swap(iterator_base& that) noexcept;
+
+      using Sims1::iterator_base<Sims2>::stats;
+    };  // class iterator_base
+
+   public:
+    using iterator        = Sims1::Iterator<iterator_base>;
+    using thread_iterator = Sims1::ThreadIterator<iterator_base>;
+    using thread_runner   = Sims1::ThreadRunner<thread_iterator>;
+
+    using Sims1Settings::cbegin_long_rules;
+    using Sims1Settings::exclude;
+    using Sims1Settings::include;
+    using Sims1Settings::number_of_threads;
+    using Sims1Settings::presentation;
+
+    [[nodiscard]] iterator cbegin(size_type n) const {
+      return SimsBase<Sims2>::cbegin<iterator>(n);
+    }
+
+    [[nodiscard]] iterator cend(size_type n) const {
+      return SimsBase<Sims2>::cend<iterator>(n);
+    }
+
+    using SimsBase<Sims2>::number_of_congruences;
+    using SimsBase<Sims2>::for_each;
+    using SimsBase<Sims2>::find_if;
+  };
+}  // namespace libsemigroups
+#endif  // LIBSEMIGROUPS_SIMS2_HPP_
---
+++ src/sims2.cpp
@@ -0,0 +1,166 @@
+//
+// libsemigroups - C++ library for semigroups and monoids
+// Copyright (C) 2023 James D. Mitchell
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// This file contains a declaration of a class for performing the "low-index
+// congruence" algorithm for semigroups and monoid.
+
+#include "libsemigroups/sims2.hpp"
+
+namespace libsemigroups {
+  ///////////////////////////////////////////////////////////////////////////////
+  // iterator_base nested class
+  ///////////////////////////////////////////////////////////////////////////////
+
+  Sims2::iterator_base::iterator_base(Sims2 const* s, size_type n)
+      : Sims1::iterator_base<Sims2>(s, n),
+        // protected
+        _2_sided_include(),
+        _2_sided_words() {
+    // TODO could be slightly less space allocated here
+    size_t const m = Sims1::iterator_base<Sims2>::maximum_number_of_classes();
+    Presentation<word_type> const& p = this->_felsch_graph.presentation();
+    _2_sided_include.assign(2 * m * p.alphabet().size(), word_type());
+    _2_sided_words.assign(n, word_type());
+  }
+
+  Sims2::iterator_base::iterator_base(Sims2::iterator_base const& that)
+      : Sims1::iterator_base<Sims2>(that),
+        _2_sided_include(that._2_sided_include),
+        _2_sided_words(that._2_sided_words) {}
+
+  Sims2::iterator_base::iterator_base(Sims2::iterator_base&& that)
+      : Sims1::iterator_base<Sims2>(
+          std::move(that)),  // TODO std::move correct?
+        _2_sided_include(std::move(that._2_sided_include)),
+        _2_sided_words(std::move(that._2_sided_words)) {}
+
+  typename Sims2::iterator_base&
+  Sims2::iterator_base::operator=(Sims2::iterator_base const& that) {
+    Sims1::iterator_base<Sims2>::operator=(that);
+    _2_sided_include = that._2_sided_include;
+    _2_sided_words   = that._2_sided_words;
+
+    return *this;
+  }
+
+  typename Sims2::iterator_base&
+  Sims2::iterator_base::operator=(Sims2::iterator_base&& that) {
+    Sims1::iterator_base<Sims2>::operator=(
+        std::move(that));  // TODO std::move ok?
+    _2_sided_include = std::move(that._2_sided_include);
+    _2_sided_words   = std::move(that._2_sided_words);
+    return *this;
+  }
+
+  void Sims2::iterator_base::swap(Sims2::iterator_base& that) noexcept {
+    Sims1::iterator_base<Sims2>::swap(that);
+    std::swap(_2_sided_include, that._2_sided_include);
+    std::swap(_2_sided_words, that._2_sided_words);
+  }
+
+  Sims2::iterator_base::~iterator_base() = default;
+
+  bool Sims2::iterator_base::try_define(PendingDef const& current) {
+    LIBSEMIGROUPS_ASSERT(current.target < current.num_nodes);
+    LIBSEMIGROUPS_ASSERT(current.num_nodes <= maximum_number_of_classes());
+
+    size_type start = current.num_edges;
+
+    if (!Sims1::iterator_base<Sims2>::try_define(current)) {
+      return false;
+    }
+
+    size_type prev_num_non_tree_edges;
+
+    // if (current.target_is_new_node) {
+    //   // number of tree edges is number_of_active_nodes - 2
+    //   // because the active nodes include the target which is a new node
+    //   prev_num_non_tree_edges = 2
+    //                             * ((_felsch_graph.definitions().size()
+    //                                 - _felsch_graph.number_of_active_nodes())
+    //                                + 2);
+    //   LIBSEMIGROUPS_ASSERT(_felsch_graph.definitions().size()
+    //                            - (prev_num_non_tree_edges / 2)
+    //                        == _felsch_graph.number_of_active_nodes() - 2);
+    // } else {
+    // number of tree edges is number_of_active_nodes - 1 because the
+    // target is not a new node
+    prev_num_non_tree_edges = 2
+                              * ((_felsch_graph.definitions().size()
+                                  - _felsch_graph.number_of_active_nodes())
+                                 + 1);
+    LIBSEMIGROUPS_ASSERT(_felsch_graph.definitions().size()
+                             - (prev_num_non_tree_edges / 2)
+                         == _felsch_graph.number_of_active_nodes() - 1);
+    //}
+
+    LIBSEMIGROUPS_ASSERT(prev_num_non_tree_edges / 2
+                         <= _felsch_graph.definitions().size());
+
+    if (current.target_is_new_node) {
+      LIBSEMIGROUPS_ASSERT(current.target < _2_sided_include.size());
+      LIBSEMIGROUPS_ASSERT(current.source < _2_sided_include.size());
+      _2_sided_words[current.target] = _2_sided_words[current.source];
+      _2_sided_words[current.target].push_back(current.generator);
+    }
+    // TODO avoid extra copies here
+    // One relation in _2_sided_include for every non-tree edge
+    while (start < _felsch_graph.definitions().size()) {
+      for (size_t i = start, j = 0; i < _felsch_graph.definitions().size();
+           ++i) {
+        auto e = _felsch_graph.definitions()[i];  // TODO reference
+        if (current.target_is_new_node && e.first == current.source
+            && e.second == current.generator) {
+          continue;
+        }
+        LIBSEMIGROUPS_ASSERT(prev_num_non_tree_edges + 2 * j + 1
+                             < _2_sided_include.size());
+        _2_sided_include[prev_num_non_tree_edges + 2 * j]
+            = _2_sided_words[e.first];
+        _2_sided_include[prev_num_non_tree_edges + 2 * j].push_back(e.second);
+        _2_sided_include[prev_num_non_tree_edges + 2 * j + 1]
+            // TODO target_no_checks
+            = _2_sided_words[_felsch_graph.target(e.first, e.second)];
+        j++;
+      }
+      // TODO different things if current.target is a new node
+
+      size_t num_non_tree_edges
+          = 2
+            * (_felsch_graph.definitions().size()
+               - _felsch_graph.number_of_active_nodes() + 1);
+
+      auto first              = _2_sided_include.cbegin();
+      auto last               = _2_sided_include.cbegin() + num_non_tree_edges;
+      start                   = _felsch_graph.definitions().size();
+      prev_num_non_tree_edges = num_non_tree_edges;
+
+      if (!felsch_graph::make_compatible<RegisterDefs>(
+              _felsch_graph,
+              0,
+              _felsch_graph.number_of_active_nodes(),
+              first,
+              last)
+          || !_felsch_graph.process_definitions(start)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}  // namespace libsemigroups
