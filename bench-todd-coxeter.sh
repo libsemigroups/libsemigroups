ee82db5e7e498b1d1941ac62724a10770083588f
James D. Mitchell <jdm3@st-andrews.ac.uk>
2024-02-13 17:11:43 +0000

    More Todd-Coxeter benchmarks

---
+++ bench-todd-coxeter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+
+for monoid in orientation_preserving_monoid \
+              orientation_reversing_monoid \
+              partition_monoid \
+              dual_symmetric_inverse_monoid \
+              block_bijection_monoid \
+              temperley_lieb_monoid \
+              singular_brauer_monoid \
+              stylic_monoid \
+              stellar_monoid; do
+  ./bench_todd_coxeter "[$monoid]" --reporter=xml | tee $monoid.xml
+done 
