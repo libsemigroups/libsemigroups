4733f591b3ce47d061c20c1576092e436731f926
reiniscirpons <rc234@st-andrews.ac.uk>
2024-04-18 15:23:49 +0100

    Add make-doc script, remove old doc scripts, modify Makefile to call make-doc script.

--- etc/make-doc.sh
+++ etc/make-doc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Generating sphinx doc from docs/yml . . ." 
 mkdir -p docs/build
 cd docs/
 echo "doxygen --version"
 doxygen --version
-./generate_from_yml.py 
+doxygen
+echo "Deploying doc fixups . . ."
+./fixup.py
