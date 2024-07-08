786be71005e05475b3c2f19f13068cb87ceb1e41
Joseph Edwards <josephdavidedwards@gmail.com>
2024-07-08 20:10:34 +0100

    Add tpp

--- include/libsemigroups/to-presentation.hpp
+++ include/libsemigroups/to-presentation.hpp
@@ -16,8 +16,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-// TODO(later) tpp file
-
 #ifndef LIBSEMIGROUPS_TO_PRESENTATION_HPP_
 #define LIBSEMIGROUPS_TO_PRESENTATION_HPP_
 
@@ -60,29 +58,7 @@ namespace libsemigroups {
   //! \exceptions
   //! \no_libsemigroups_except
   template <typename WordOutput>
-  // TODO(later) why not just to_presentation with no template WordOutput,
-  // always use word_type, and can be converted after if desirable
-  Presentation<WordOutput> to_presentation(FroidurePinBase& fp) {
-    Presentation<WordOutput> p;
-    p.alphabet(fp.number_of_generators());
-    WordOutput lhs, rhs;
-    auto       f = [](auto val) {
-      return presentation::human_readable_letter<WordOutput>(val);
-    };
-
-    for (auto it = fp.cbegin_rules(); it != fp.cend_rules(); ++it) {
-      lhs.resize(it->first.size());
-      rhs.resize(it->second.size());
-      std::transform(it->first.cbegin(), it->first.cend(), lhs.begin(), f);
-      std::transform(it->second.cbegin(), it->second.cend(), rhs.begin(), f);
-      p.rules.push_back(std::move(lhs));
-      p.rules.push_back(std::move(rhs));
-      lhs.clear();
-      rhs.clear();
-    }
-
-    return p;
-  }
+  Presentation<WordOutput> to_presentation(FroidurePinBase& fp);
 
 #ifdef PARSED_BY_DOXYGEN
   //! \ingroup to_presentation_group
@@ -122,28 +98,7 @@ namespace libsemigroups {
                               typename Presentation<WordInput>::letter_type>
           && !std::is_same_v<WordOutput, WordInput>>>
   Presentation<WordOutput> to_presentation(Presentation<WordInput> const& p,
-                                           Func&&                         f) {
-    p.validate();
-    // Must call p.validate otherwise f(val) may segfault if val is not in the
-    // alphabet
-
-    Presentation<WordOutput> result;
-    result.contains_empty_word(p.contains_empty_word());
-    WordOutput new_alphabet;
-    new_alphabet.resize(p.alphabet().size());
-    std::transform(
-        p.alphabet().cbegin(), p.alphabet().cend(), new_alphabet.begin(), f);
-    // TODO(later) use alphabet_no_checks when it is implemented
-    result.alphabet(new_alphabet);
-    WordOutput rel;
-    for (auto it = p.rules.cbegin(); it != p.rules.cend(); ++it) {
-      rel.resize(it->size());
-      std::transform(it->cbegin(), it->cend(), rel.begin(), f);
-      result.rules.push_back(std::move(rel));
-      rel.clear();
-    }
-    return result;
-  }
+                                           Func&&                         f);
 #endif
 
 #ifdef PARSED_BY_DOXYGEN
@@ -178,11 +133,7 @@ namespace libsemigroups {
   template <typename WordOutput, typename WordInput>
   auto to_presentation(Presentation<WordInput> const& p)
       -> std::enable_if_t<!std::is_same_v<WordOutput, WordInput>,
-                          Presentation<WordOutput>> {
-    return to_presentation<WordOutput>(p, [&p](auto val) {
-      return presentation::human_readable_letter<WordOutput>(p.index(val));
-    });
-  }
+                          Presentation<WordOutput>>;
 #endif
 
 #ifdef PARSED_BY_DOXYGEN
@@ -226,18 +177,7 @@ namespace libsemigroups {
                               typename Presentation<WordInput>::letter_type>
           && !std::is_same_v<WordOutput, WordInput>>>
   InversePresentation<WordOutput>
-  to_inverse_presentation(InversePresentation<WordInput> const& ip, Func&& f) {
-    ip.validate_word(ip.inverses().begin(), ip.inverses().end());
-    InversePresentation<WordOutput> result(
-        std::move(to_presentation<WordOutput>(ip, f)));
-
-    WordOutput new_inverses;
-    new_inverses.resize(ip.inverses().size());
-    std::transform(
-        ip.inverses().cbegin(), ip.inverses().cend(), new_inverses.begin(), f);
-    result.inverses_no_checks(new_inverses);
-    return result;
-  }
+  to_inverse_presentation(InversePresentation<WordInput> const& ip, Func&& f);
 #endif
 
   //! \ingroup to_presentation_group
@@ -259,19 +199,14 @@ namespace libsemigroups {
   //! \throws LibsemigroupsException if `p.validate()` throws.
   template <typename Word>
   InversePresentation<Word>
-  to_inverse_presentation(Presentation<Word> const& p) {
-    InversePresentation<Word> result(p);
-    presentation::normalize_alphabet(result);
-    result.alphabet(2 * result.alphabet().size());
-    auto invs = result.alphabet();
-    std::rotate(invs.begin(), invs.begin() + invs.size() / 2, invs.end());
-    result.inverses_no_checks(std::move(invs));
-    return result;
-  }
+  to_inverse_presentation(Presentation<Word> const& p);
 
   //! No doc
   // TODO(now) is this the correct way to prevent the nonsensical conversion of
   // an inverse presentation to an inverse presentation of the same type?
+  // JDE wanted some way of preventing the user from calling
+  // to_inverse_presentation(Presentaion<Word> const& p) in the case where p is,
+  // in fact, already an inverse presentation.
   template <typename Word>
   InversePresentation<Word>
   to_inverse_presentation(InversePresentation<Word> const& ip) {
@@ -314,12 +249,11 @@ namespace libsemigroups {
   template <typename WordOutput, typename WordInput>
   auto to_inverse_presentation(InversePresentation<WordInput> const& ip)
       -> std::enable_if_t<!std::is_same_v<WordOutput, WordInput>,
-                          InversePresentation<WordOutput>> {
-    return to_inverse_presentation<WordOutput>(ip, [&ip](auto val) {
-      return presentation::human_readable_letter<WordOutput>(ip.index(val));
-    });
-  }
+                          InversePresentation<WordOutput>>;
 #endif
 
 }  // namespace libsemigroups
+
+#include "to-presentation.tpp"
+
 #endif  // LIBSEMIGROUPS_TO_PRESENTATION_HPP_
