f589fff76280e97de36a05126e7780d317d49b04
Finn Smith <fls3@st-andrews.ac.uk>
2024-08-14 15:52:28 +0100

    fix TOOD

--- tests/test-konieczny-transf.cpp
+++ tests/test-konieczny-transf.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-// TOOD(later) replace Transf(...) with Transf::make
+// TODO(later) replace Transf(...) with Transf::make
 
 #include <cstddef>  // for size_t
 
