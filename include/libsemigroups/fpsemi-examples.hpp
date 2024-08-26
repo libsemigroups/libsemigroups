f0af1e61ab3fb5b7825bdfeac4231d49bd1c0c78
Murray Whyte <mw231@st-andrews.ac.uk>
2024-08-26 13:28:57 +0100

    Fix documentation spacing thing

--- include/libsemigroups/fpsemi-examples.hpp
+++ include/libsemigroups/fpsemi-examples.hpp
@@ -456,14 +456,6 @@ namespace libsemigroups {
     //! * `author::Mitchell + author::Whyte` (see Theorem 1.4 of
     //! [10.48550/arXiv.2406.19294][])
     //!
-    // When val == author::Gay, this is just a presentation for the symmetric
-    // inverse monoid, a slightly modified version from Solomon (so that
-    // contains the Coxeter+Moser presentation for the symmetric group),
-    // Example 7.1.2 in Joel gay's thesis (JDM the presentation in Example 7.1.2
-    // seems to have 2n - 1 generators whereas this function returns a monoid on
-    // n generators. TODO ask Florent again if this reference is correct
-    // Maybe should be Solomon:
-    // https://www.sciencedirect.com/science/article/pii/S0021869303005933/pdf
     //!
     //! The default value for `val` is `author::Mitchell + author::Whyte`.
     //!
@@ -477,6 +469,15 @@ namespace libsemigroups {
     //!
     //! [10.1007/978-1-84800-281-4]: https://doi.org/10.1007/978-1-84800-281-4
     //! [10.48550/arXiv.2406.19294]: https://doi.org/10.48550/arXiv.2406.19294
+    //
+    // When val == author::Gay, this is just a presentation for the symmetric
+    // inverse monoid, a slightly modified version from Solomon (so that
+    // contains the Coxeter+Moser presentation for the symmetric group),
+    // Example 7.1.2 in Joel gay's thesis (JDM the presentation in Example 7.1.2
+    // seems to have 2n - 1 generators whereas this function returns a monoid on
+    // n generators. TODO ask Florent again if this reference is correct
+    // Maybe should be Solomon:
+    // https://www.sciencedirect.com/science/article/pii/S0021869303005933/pdf
     [[nodiscard]] Presentation<word_type>
     symmetric_inverse_monoid(size_t n,
                              author val = author::Mitchell + author::Whyte);
