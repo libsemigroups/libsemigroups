a2f1342722b91cf7461ab97f8867e1b1a53a844e
Joseph Edwards <josephdavidedwards@gmail.com>
2024-10-23 16:22:29 +0100

    Disable some linting

--- tests/test-hpcombi.cpp
+++ tests/test-hpcombi.cpp
@@ -293,11 +293,13 @@ namespace libsemigroups {
         {Renner0Element({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}),
           Renner0Element(
              {FF, FF, FF, FF, FF, FF, FF, FF, 8, 9, 10, 11, 12, 13, 14, 15}),
+          // NOLINTBEGIN(whitespace/line_length)
           Renner0Element({0, 1, 2, 3, 4, 5, 6, 8, 7, 9, 10, 11, 12, 13, 14, 15}),
           Renner0Element({0, 1, 2, 3, 4, 5, 7, 6, 9, 8, 10, 11, 12, 13, 14, 15}),
           Renner0Element({0, 1, 2, 3, 4, 6, 5, 7, 8, 10, 9, 11, 12, 13, 14, 15}),
           Renner0Element({0, 1, 2, 3, 5, 4, 6, 7, 8, 9, 11, 10, 12, 13, 14, 15}),
           Renner0Element({0, 1, 2, 4, 3, 5, 6, 7, 8, 9, 10, 12, 11, 13, 14, 15}),
+          // NOLINTEND
           Renner0Element(
              {0, 1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 13, 12, 14, 15})});
     // Note that the number in this test file was 8962225 since (at least) this
