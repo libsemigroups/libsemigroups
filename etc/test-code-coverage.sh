ae44ce186a5d4154aec635a05e9d20653082d3a3
Joseph Edwards <josephdavidedwards@gmail.com>
2025-01-06 20:12:29 +0000

    Suppress gcov errors

--- etc/test-code-coverage.sh
+++ etc/test-code-coverage.sh
@@ -97,8 +97,8 @@ fi
 
 bold "Running lcov and genhtml . . .";
 printf "\033[2m";
-lcov  --directory . --capture --output-file "coverage.info.tmp" --test-name "libsemigroups_1_0_0" --no-checksum --no-external --compat-libtool --gcov-tool "gcov" | grep -v "ignoring data for external file"
-lcov  --directory . --remove "coverage.info.tmp" "/tmp/*" "/Applications/*" --output-file "coverage.info"
+lcov  --ignore-errors mismatch,negative --directory . --capture --output-file "coverage.info.tmp" --test-name "libsemigroups_1_0_0" --no-checksum --no-external --compat-libtool --gcov-tool "gcov" | grep -v "ignoring data for external file"
+lcov  --ignore-errors unused --directory . --remove "coverage.info.tmp" "/tmp/*" "/Applications/*" --output-file "coverage.info"
 LANG=C genhtml  --prefix . --output-directory "coverage" --title "libsemigroups Code Coverage" --legend --show-details "coverage.info.tmp"
 rm -f coverage.info.tmp
 printf "\033[0m";
