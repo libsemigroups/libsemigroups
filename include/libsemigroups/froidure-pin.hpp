25e0d0e9c3a6a0913444457edb75fb4dbd9e9882
James D. Mitchell <jdm3@st-andrews.ac.uk>
2025-02-21 16:02:52 +0000

    TODO -> DONE

--- include/libsemigroups/froidure-pin.hpp
+++ include/libsemigroups/froidure-pin.hpp
@@ -313,7 +313,7 @@ namespace libsemigroups {
    private:
     template <typename T>
     static constexpr bool IsState
-        = ((!std::is_void_v<T>) &&std::is_same_v<state_type, T>);
+        = ((!std::is_void_v<T>) && std::is_same_v<state_type, T>);
 
     ////////////////////////////////////////////////////////////////////////
     // FroidurePin - data - private
@@ -1381,8 +1381,8 @@ namespace libsemigroups {
 
     void expand(size_type);
     void is_one(internal_const_element_type x, element_index_type) noexcept(
-        std::is_nothrow_default_constructible_v<InternalEqualTo>&& noexcept(
-            std::declval<InternalEqualTo>()(x, x)));
+        std::is_nothrow_default_constructible_v<InternalEqualTo>
+        && noexcept(std::declval<InternalEqualTo>()(x, x)));
 
     void copy_generators_from_elements(size_t);
     void closure_update(element_index_type,
@@ -1627,7 +1627,8 @@ namespace libsemigroups {
     //!
     //! \warning This function does not check its arguments. In particular, it
     //! is assumed that the generators in \p gens all have the same degree.
-    // TODO(0) shouldn't this be called make?
+    // NOTE: this isn't called make because it is not constructing anything,
+    // rather re-initializing.
     template <typename Container>
     FroidurePin<typename Container::value_type>&
     init(FroidurePin<typename Container::value_type>& fp,
