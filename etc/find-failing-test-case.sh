3e5cc69d476a7b4c782d160a7228675bea48ccc2
James D. Mitchell <jdm3@st-andrews.ac.uk>
2025-02-25 15:48:15 +0000

    Remove multiple old files

--- etc/find-failing-test-case.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-exclude="[exclude:standard][exclude:extreme][exclude:fail][exclude:no-valgrind][exclude:no-coverage]"
-
-for i in $(seq -f "%03g" 0 117); do  
-  ./test_todd_coxeter "[$i]$exclude"
-done
