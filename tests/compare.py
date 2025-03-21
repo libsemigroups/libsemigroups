3b152f4e75935af83d4a1b6a18983e1d6cd0f614
James D. Mitchell <jdm3@st-andrews.ac.uk>
2025-03-21 14:34:57 +0000

    Rm accidental file

--- tests/compare.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-A simple script to compare two test runs.
-"""
-
-# platform.processor() -> 'macOS-14.5-arm64-arm-64bit'
-# repo = git.Repo(search_parent_directories=True)
-# sha = repo.head.object.hexsha
