24b772772272dd8f738e6874434b739317f8b28a
Nadim Searight <ns241@st-andrews.ac.uk>
2025-04-14 11:10:59 +0100

    moved is_strictly_cyclic. Compiles and tests pass

--- include/libsemigroups/word-graph-helpers.hpp
+++ include/libsemigroups/word-graph-helpers.hpp
@@ -26,10 +26,11 @@ namespace libsemigroups {
     [[nodiscard]] bool is_strictly_cyclic(WordGraphView<Node> const& wg);
 
     template <typename Node>
-    [[nodiscard]] bool is_strictly_cyclic(WordGraphView<Node> const& wg) {
+    [[nodiscard]] bool is_strictly_cyclic(WordGraph<Node> const& wg) {
       return is_strictly_cyclic(WordGraphView<Node>(wg));
     }
-  }  // namespace libsemigroups
+  }  // namespace word_graph
+}  // namespace libsemigroups
 
 #include "libsemigroups/word-graph-helpers.tpp"
 #endif
