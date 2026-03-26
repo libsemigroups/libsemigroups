17be949e2f07cb501b56b1beaaf3d9d71d5372b6
James Mitchell <jdm3@st-andrews.ac.uk>
2026-03-26 12:05:52 +0000

    rebuilding site Thu 26 Mar 2026 12:05:52 GMT

--- search/all_4.js
+++ search/all_4.js
@@ -6,5 +6,6 @@ var searchData=
   ['4_20released_2004_2003_202022_3',['v2.1.4 (released 04/03/2022)',['../md_changelog-v2.html#v214-released-04032022',1,'']]],
   ['4_20released_2011_2001_202020_4',['v1.0.4 (released 11/01/2020)',['../md_changelog-v1.html#v104-released-11012020',1,'']]],
   ['4_20released_2018_2006_202025_5',['2.7.4 (released 18/06/2025)',['../md_changelog-v2.html#autotoc_md274-released-18062025',1,'']]],
-  ['4_20released_2027_2011_202020_6',['v1.3.4 (released 27/11/2020)',['../md_changelog-v1.html#v134-released-27112020',1,'']]]
+  ['4_20released_2026_2003_202026_6',['v3.5.4 (released 26/03/2026)',['../md_changelog-v3.html#v354-released-26032026',1,'']]],
+  ['4_20released_2027_2011_202020_7',['v1.3.4 (released 27/11/2020)',['../md_changelog-v1.html#v134-released-27112020',1,'']]]
 ];
